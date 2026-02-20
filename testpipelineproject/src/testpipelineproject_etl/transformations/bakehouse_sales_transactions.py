d59e4c100894abfbc026a3c3d6d3d6b9981cf866
heatheranndye@gmail.com <heatheranndye@gmail.com>
2026-02-20 15:14:56 +0000

    commit initial files

---
+++ testpipelineproject/src/testpipelineproject_etl/transformations/bakehouse_sales_transactions.py
@@ -0,0 +1,10 @@
+from pyspark import pipelines as dp
+from pyspark.sql import functions as F
+
+
+@dp.table
+def bakehouse_sales_transactions():
+    return( spark.read.table("samples.bakehouse.sales_transactions")
+    .select("transactionID", "franchiseID", "totalPrice" ))
+
+    
